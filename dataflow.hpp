2f841db84927b4573b80e85f1aa35ec561a6304c
Andrew Smith <andrew.smith@miracl.com>
2017-05-17 21:28:41 +0100

    switch to shared_ptr

--- dataflow.hpp
+++ dataflow.hpp
@@ -8,7 +8,8 @@
 #include <algorithm>
 #include <utility>
 #include <future>
-
+#include <experimental/optional>
+#include <memory>
 
 namespace dataflow {
 
@@ -17,78 +18,77 @@ namespace dataflow {
 // TODO: Add unit test framework
 
 
+
+
+
+
 class Node {
 
     public:
         virtual ~Node() {};
 
         virtual void clear() = 0;
-        virtual bool isSet() const = 0;
+        virtual bool has_value() const = 0;
 };
 
-template<typename T> class Value :
-    public Node {
+template<typename T> class Value : public Node {
 
-    T _value = {};
-    bool _isSet = false;
+    std::experimental::optional<T> _value;
+    std::experimental::optional<std::exception> _e;
 
     public:
-        Value() {
 
+        void set(const std::exception& e) {
+            _e = e;
+            _value = {};
         }
 
-        Value ( Value&& ) = delete;
-        Value ( const Value& ) = delete;
-
-        Value(const T& value) {
+        void set1(const T& value) {
+            _e = {};
             _value = value;
-            _isSet = true;
         }
 
         T get() const {
-            if(!_isSet) {
-                throw std::logic_error("not set");
+            if(_e) {
+                throw _e;
             }
 
-            return _value;
-        }
-
-        void set(const T& value) {
-            _value = value;
-            _isSet = true;
+            return _value.value();
         }
 
         virtual void clear() {
-            _isSet = false;
+            _e = {};
+            _value = {};
         }
 
-        virtual bool isSet() const {
-            return _isSet;
+        virtual bool has_value() const {
+            return (bool)_value;
         }
 };
 
 class graph {
 
     typedef std::function<void()> Calculator;
-    std::map<const Node*, Calculator> _functions;
+    std::map<std::shared_ptr<Node>, Calculator> _functions;
 
-    typedef std::vector<const Node*> NodeList;
-    std::map<const Node*, NodeList> _values;
+    typedef std::vector<std::shared_ptr<Node>> NodeList;
+    std::map<std::shared_ptr<Node>, NodeList> _values;
 
-    std::map<const Node*, int> _nodeLevels;
+    std::map<std::shared_ptr<Node>, int> _nodeLevels;
     std::map<int, NodeList> _levels;
 
     private:
-        template<typename F, typename R, typename... V> auto bind(const F& f, Value<R>* r, const Value<V>* ... v) {
-            return Calculator([f, r, v...] () {r->set(f(v->get()...));});
+        template<typename F, typename R, typename... V> auto bind(const F& f, std::shared_ptr<Value<R>> r, std::shared_ptr<Value<V>> ... v) {
+            return Calculator([f, r, v...] () {r->set1(f(v->get()...));});
         }
 
     public:
-        template<typename F, typename ...V> auto attach(const F& f, const Value<V>* ... v) {
-            typedef Value<decltype(f(v->get()...))> R;
-            auto* r = new R{};
+        template<typename F, typename ...V> auto attach(const F& f, std::shared_ptr<Value<V>> ... v) {
+            typedef decltype(f(v->get()...)) R;
+            auto r = std::make_shared<Value<R>>();
             _functions[r] = bind(f, r, v...);
-            const auto& args = _values[r] = std::initializer_list<const Node*>{v...};
+            auto rn = std::static_pointer_cast<Node>(r);
+            const auto& args = _values[rn] = std::initializer_list<std::shared_ptr<Node>>{std::static_pointer_cast<Node>(v)...};
 
             // TODO assert all args are in the nodes of this graph
 
@@ -101,62 +101,7 @@ class graph {
             return r;
         }
 
-        size_t pass() {
-            size_t remaining = 0;
-            for(auto& kv: _functions) {
-                const auto& result = kv.first;
-                const auto& f = kv.second;
-                const auto& args = _values[result];
-
-                if(!result->isSet()) {
-                    if(std::all_of(args.cbegin(), args.cend(), [](auto arg) {return arg->isSet();})){
-                        f();
-                    } else {
-                        remaining++;
-                    }
-                }
-            }
-
-            // all values are calculated
-            return remaining;
-        }
-
-        void repeated() {
-            while(pass() > 0) {
-                // repeat
-            }
-        }
-
-        void recursive_bottomup() {
-
-            std::function<void(const Node*)> calculate;
-            calculate = [this, &calculate] (const Node* value) {
-                if(value->isSet())
-                    return;
-
-                const auto& f = _functions[value];
-                const auto& args = _values[value];
-                std::for_each(args.cbegin(), args.cend(), calculate);
-                f();
-            };
-
-            for(const auto& kv: _functions) {
-                auto value = kv.first;
-                calculate(value);
-            }
-        }
-
-        void recursive_topdown() {
-
-        }
-
-        void stack_bottomup() {
-        }
-
-        void stack_topdown() {
-        }
-
-        void by_level() {
+        void calculate() {
             for(const auto& level : _levels) {
                 const auto& nodes = level.second;
                 std::for_each(nodes.cbegin(), nodes.cend(),
@@ -166,7 +111,7 @@ class graph {
             }
         }
 
-        void multithreaded_level_synchronous() {
+        void calculate_multithreaded() {
             for(const auto& level : _levels) {
                 const auto& nodes = level.second;
                 std::vector<std::future<void>> tasks;
--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <experimental/optional>
 #include "dataflow.hpp"
-
 using namespace std;
+
+
+// nodes can hold a value of type either<optional<T>, std::exception>
+// root arguments are given to us as optional<T>
+//
+
+
+
+/*
+
+    try {
+        return Value(f());
+    } catch(const std::exception& e) {
+        return Value(std::optional<T>(), e);
+    }
+
+
+*/
+
+struct X {
+    X() = delete;
+};
+
+typedef std::experimental::optional<X> x;
 
 
 int main()
@@ -29,14 +53,14 @@ int main()
     auto spread = g.attach([]() {return 0.1;});
     auto shift = g.attach([]() {return 0.5;});
 
-    std::map<std::string, dataflow::Value<Quote>*> quotes;
+    std::map<std::string, std::shared_ptr<dataflow::Value<Quote>>> quotes;
     quotes["1"] = g.attach(fquote, mid, spread);
     quotes["2"] = g.attach(fwiden, quotes["1"], spread);
     quotes["3"] = g.attach(fwiden, quotes["2"], spread);
     quotes["4"] = g.attach(fshift, quotes["2"], shift);
     quotes["5"] = g.attach(fspan, quotes["4"], quotes["1"]);
 
-    g.multithreaded_level_synchronous();
+    g.calculate_multithreaded();
 
     for(const auto& quote: quotes) {
         cout << quote.first << ":" << quote.second->get().first << "," << quote.second->get().second << endl;
